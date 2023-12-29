f8d8e2f94fba71eb4fee1ab16bcf1e10ea158338
User <User@DESKTOP-7I80P5L>
2023-12-29 10:32:23 +0530

    All the OOp Principles

---
+++ Oopprinciple/src/oop/Class1.java
@@ -0,0 +1,15 @@
+package oop;
+
+public class Class1 implements Interface1 
+{
+	
+	public void country(String cname)
+	{
+		System.out.println("Name of Country:  "+cname);
+	}
+	
+	public void city(String cname)
+	{
+		System.out.println("Name of City: "+cname);
+	}
+}
---
+++ Oopprinciple/src/oop/Interface1.java
@@ -0,0 +1,11 @@
+package oop;
+
+public interface Interface1
+{
+	void country(String s);
+	void city(String s);
+	public static void show()
+	{
+		System.out.println("Your location is added!!");
+	}
+}
